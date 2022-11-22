1b9699a421fcbfc19fadc9a7c316bafe158b0d04
iseiseje <cogaptek.pinem@gmail.com>
2022-11-22 10:29:02 +0700

    Create input.php

---
+++ input.php
@@ -0,0 +1,33 @@
+<body>
+	<div class="judul">		
+		<h1Input Ke Database</h1>
+	</div>
+		<br/>
+ 	<a href="index.php">Lihat Semua Data</a>
+ 	<br/>
+	<h3>Input data baru</h3>
+	<form action="inputact.php" method="post">		
+		<table>
+			<tr>
+				<td>ID</td>
+				<td><input type="text" name="id"></td>					
+			</tr>	
+      <tr>
+				<td>Nama</td>
+				<td><input type="text" name="nama"></td>					
+			</tr>	
+			<tr>
+				<td>Alamat</td>
+				<td><input type="text" name="alamat"></td>					
+			</tr>	
+			<tr>
+				<td>Pekerjaan</td>
+				<td><input type="text" name="pekerjaan"></td>					
+			</tr>	
+			<tr>
+				<td></td>
+				<td><input type="submit" value="Simpan"></td>					
+			</tr>				
+		</table>
+	</form>
+</body>
